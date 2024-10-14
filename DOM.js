51c81cad43bd2d9fedce96cfa9467fa73b82f0db
KaniC-Web <kanimozhichandrasekhar05@gmail.com>
2024-10-14 10:59:39 +0100

    Update DOM.js

--- DOM.js
+++ DOM.js
@@ -1,11 +1,9 @@
 document.getElementById('submitButton').addEventListener('click', function() {
-    // Get the input field value
+   
     const inputValue = document.getElementById('inputField').value;
 
-    // Update the result div with the input value
     document.getElementById('result').innerText = `You entered: ${inputValue}`;
 
-    // Hide the input field and button
     document.getElementById('inputField').style.display = 'none';
     document.getElementById('submitButton').style.display = 'none';
 });
